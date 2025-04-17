39b0d92977af78132de1fa97547864e46ad51ec6
Jatin Sharma <141317836+Jatin-Purohit@users.noreply.github.com>
2025-04-17 17:28:07 +0530

    Title Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,46 +3,42 @@ import { League_Spartan } from "next/font/google"; // Importing the available Go
 import "./globals.css";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
-import ScrollToTopButton from "../../components/Scrolltotop"; 
+import ScrollToTopButton from "../../components/Scrolltotop";
 import SocialIcon from "../../components/SocialMediaIcons";
 import PopupModal from "../../components/PopUp";
 
-
-
 const leagueSpartan = League_Spartan({
-    subsets: ["latin"],
-    weight: ["100", "400", "900"], // Adjust as needed
-    display: "swap",
+  subsets: ["latin"],
+  weight: ["100", "400", "900"], // Adjust as needed
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-    title: "Civil Mantra",
-    description: "",
+  title: "CivilMantra",
+  description: "",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className={leagueSpartan.className}> 
-            <head>
-                
-                <link 
-                  rel="stylesheet" 
-                  href="https://fonts.googleapis.com/css2?family=Kablammo&family=League+Spartan:wght@100..900&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap" 
-                />
-            </head>
-            <body className="font-league-spartan">
-                <PopupModal/>
-                <NavBar />
-                <SocialIcon/>
-                {children}
-                <Footer />
-                <ScrollToTopButton /> 
-                
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" className={leagueSpartan.className}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Kablammo&family=League+Spartan:wght@100..900&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap"
+        />
+      </head>
+      <body className="font-league-spartan">
+        <PopupModal />
+        <NavBar />
+        <SocialIcon />
+        {children}
+        <Footer />
+        <ScrollToTopButton />
+      </body>
+    </html>
+  );
 }
